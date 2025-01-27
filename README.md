97900d3a48019e33ed624b80c03489ddf39db68f
Madhushika Bandara <79932497+Madhushika-Bandara@users.noreply.github.com>
2025-01-27 21:38:28 +0530

    Updated ReadMe.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # To-Do-Web-App
  React Node Express MySQL
+
+# Frontend
+ to install packages -> npm install
+ to run the frontend -> npm run dev
+
+# Backend
+ to install packages -> npm install
+ to run the backend -> npm start
+
+# Database
+  use Xampp and PHPmyadmin to access the databae.
--- db/todo_db.sql
+++ db/todo_db.sql
@@ -12,18 +12,9 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `todo_db`
---
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `todos`
 --
 
@@ -49,26 +40,11 @@ INSERT INTO `todos` (`id`, `title`, `description`, `is_done`, `created_at`) VALU
 (20, 'Go for a run.', 'Run around the block for 30 minutes and walk for 15 minutes.', 0, '2025-01-27 14:13:19');
 
 --
--- Indexes for dumped tables
---
-
---
--- Indexes for table `todos`
---
 ALTER TABLE `todos`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `todos`
 --
 ALTER TABLE `todos`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
